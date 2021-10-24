a20fba34eeb479eba35990085a3a1fedec04f410
erikalilianar22 <00106416@uca.edu.sv>
2021-10-23 19:52:35 -0600

    principal

--- routes/home.js
+++ routes/home.js
@@ -1,36 +1,4 @@
 var express = require('express');
 var router = express.Router();
 
-const palabras=[
-    {nombre: 'gato'},
-    {nombre: 'pluton'},
-    {nombre: 'arbol'},
-    {nombre: 'piedra'}
-]
-
-const formulario = document.querySelector('#formulario');
-const boton = document.querySelector('#boton');
-const resultado = document.querySelector('#resultado');
-
-const buscar =()=>{
-    //console.log(formulario.value);
-
-    resultado.innerHTML='';
-    const texto = formulario.value.toLowerCase();
-    for(let palabra of palabras){
-        let nombre= palabra.nombre.toLocaleLowerCase();
-        if(nombre.indexOf(texto) !== -1){
-            resultado.innerHTML += `
-            <li>${producto.nombre}</li>
-            `
-        }
-    }
-}
-
-boton.addEventListener('click',buscar)
-//formulario.addEventListener('keyup',buscar)
-
-//buscar();
-
-
 module.exports = router;
