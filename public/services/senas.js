3c1aeca58c5b88ef28434715c150d467f5d9ddbd
Drubico <00121117@uca.edu.sv>
2021-10-31 14:13:22 -0600

    {2.GET}-> AXIOS : Obteniendo todas las senas

---
+++ public/services/senas.js
@@ -0,0 +1,19 @@
+axios.get(api+'signs/getAll')
+      .then(function (response) {
+        document.getElementById('obteniendo').innerHTML = 
+        response.data.map(function (signs) 
+        {
+          return (        
+          '<div class="grid-item">'+
+            '<div class="card" style="width: 18rem;">'+
+              '<a href="sign/"><img class="card-img-top" src="'+signs.image+'" alt="Card image cap"/>'
+                +'<div class="card-body">'
+                  +'<h5 class="card-title">'+signs.name+'</h5>'
+                +'</div></a></div>'
+          +'</div>'
+          );
+        }).join('');
+        })
+        .catch(function (err) {
+          document.getElementById('obteniendo').innerHTML = '<li class="text-danger">' + err.message + '</li>';
+        });
