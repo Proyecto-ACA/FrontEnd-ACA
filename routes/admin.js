23cd4a634c251a442e36045f7c6754e5c09ae6f2
Drubico <00121117@uca.edu.sv>
2021-11-02 14:42:52 -0600

    |02/11|@|AR==>> Vistas : Split Table and button new word

--- routes/admin.js
+++ routes/admin.js
@@ -24,4 +24,11 @@ router.get('/edit', (req, res, next)=>{
   res.render('word_admin_edit', { title: 'Administrar de palabras' });
 });
 
+router.get('/add', (req, res, next)=>{
+  if(req.isAuthenticated()) return next();
+  res.redirect("/login");
+}, (req, res) => {
+  res.render('word_admin_add', { title: 'Administrar de palabras' });
+});
+
 module.exports = router;
