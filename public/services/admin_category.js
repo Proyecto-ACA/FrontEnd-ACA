99cdda27d1aaa8ce9820e3d7c498cbd682599f68
Drubico <dilverpro@gmail.com>
2021-11-09 12:39:27 -0600

    category view

--- public/services/admin_category.js
+++ public/services/admin_category.js
@@ -25,13 +25,12 @@ axios.get(api+'category/getAllOrderbyId')
         document.getElementById('mytable').innerHTML = 
         response.data.map(function (category) 
         {
-          // console.log("Sena : ",signs.name,signs)
           return (        
             '<tr>'+
                 '<th scope="row">'+category.id+'</th>'+
                     '<td>'+category.name+'</td>'+
                     '<td><img class="d-inline-block align-top" src='+category.image+' width="30" height="30" alt="" /></td>'+
-                    '<td>'+'<a class="btn btn-sm btn-warning" href="/admin/edit_category?id=' +category.id+'">Editar</a>'+'</td>'+
+                    '<td>'+'<a class="btn btn-sm btn-primary" href="/admin/edit_category?id=' +category.id+'">Editar</a>'+'</td>'+
                     '<td>'+'<a class="btn btn-sm btn-danger" onClick="deleteconfirmation('+category.id+')">Eliminar</a>'+'</td>'+
             '</tr>'
           );
@@ -40,3 +39,18 @@ axios.get(api+'category/getAllOrderbyId')
         .catch(function (err) {
           document.getElementById('mytable').innerHTML = '<li class="text-danger">' + err.message + '</li>';
         });
+
+        
+        const filtrar = () => {
+          var texto = filtro_busqueda.value.toLowerCase();
+          for (let i=0; i<mytable.childElementCount; i++){
+            let busqueda = mytable.children[i].children[1].textContent.toLowerCase();
+            if(busqueda.indexOf(texto) !== -1){
+              mytable.children[i].style.display = '' 
+          } else{
+              mytable.children[i].style.display = 'none'
+          }
+          }
+        }
+        
+        filtro_busqueda.addEventListener('keyup',filtrar);
