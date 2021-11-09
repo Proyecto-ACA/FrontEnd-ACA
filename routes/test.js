9c5b6df33f5ab29436c60a21179466ba62bf3223
luist23 <00127717@uca.edu.sv>
2021-11-08 22:23:19 -0600

    mod test

--- routes/test.js
+++ routes/test.js
@@ -1,34 +1,36 @@
+const e = require('express');
 var express = require('express');
 var router = express.Router();
-const axios = 'axios'
+const axios = require('axios');
 
 router.get('/', (req, res, next)=>{
     console.log('aqui prro');
 
     axios({
         method: 'GET',
-        url: 'localhost:3023',
+        url: 'localhost:3023/lesson/lessonsign/getAll',
         //data: req.body,
     })
-        .then((response) => {
-            //console.log('responce', response);
-            if (e) {
-                //e(response.data);
-            }
-            if (response.data.code === 4012) {
-                //console.log('responce', response.data);
-                //res.redirect('/logout');
-            } else {
-                //res.send(response.data);
-            }
-        })
-        .catch((error) => {
-        res.send(error.message);
-        console.log('No logro concetar a la direccion', error.message);
-        })
-        .then(() => {
-        //console.log('goal :v');
-    });
+    .then((response) => {
+        console.log('responce', response);
+        if (e) {
+            //e(response.data);
+            //console.log(e.data);
+            console.log('data');
+        }
+        if (response.data.code === 4012) {
+            //console.log('responce', response.data);
+            //res.redirect('/logout');
+        } else {
+            //res.send(response.data);
+        }
+        console.log('responce', response.data);
+    })
+    .catch((error) => {
+    //res.send('error.message');
+    console.log('No logro concetar a la direccion', error.message);
+    })
+console.log('aqui prrox2');
 });
 
 module.exports = router;
