9808e19e35ade62675672ab92f90b1767904a6af
Drubico <dilverpro@gmail.com>
2021-11-09 12:22:05 -0600

    get admins

--- public/services/admin_users.js
+++ public/services/admin_users.js
@@ -5,14 +5,11 @@ axios.get(api+'users/getAll')
         document.getElementById('mytable').innerHTML = 
         response.data.map(function (user) 
         {
-          // console.log("Sena : ",signs.name,signs)
           return (        
             '<tr>'+
                 '<th scope="row">'+user.id+'</th>'+
                     '<td>'+user.name+'</td>'+
                     '<td>'+user.rol_id+'</td>'+
-                    '<td>'+'<a class="btn btn-sm btn-warning" href="/admin/edit_word?id=' +signs.id+'">Editar</a>'+'</td>'+
-                    '<td>'+'<a class="btn btn-sm btn-danger" onClick="deleteconfirmation('+signs.id+')">Eliminar</a>'+'</td>'+
             '</tr>'
           );
         }).join('');
