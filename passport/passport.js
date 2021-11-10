b1cee97ce3b93f93322a53ee640982f372184ad6
Drubico <dilverpro@gmail.com>
2021-11-09 18:12:09 -0600

    🔥 Registro funcional

--- passport/passport.js
+++ passport/passport.js
@@ -84,29 +84,26 @@ module.exports = function (passport) {
             passReqToCallback: true,
         },
         function (req, username, password, done) {
-
+             var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+            console.log(hash);
             axios.post(api+'users/user',{name: username})
             .then((user) => {
-                if (user) {
-                    console.log("-----------------------------------------\nYA EXISTE\n-------------------------------")
-                    return done(null, false, {
-                        message: 'Usuario ya existe'
-                    });
-                }
-                else{
-                    var obj = new Object();
-                    obj.name = username;
-                    obj.rol_id = rol_id;
-                    obj.password = password;
-                    
-                    axios.post(api+'users/save', JSON.parse(jsonString))
-                    .then(function (res) {
-                        console.log("se envio"+res)
-                        })
-                    .catch(function (err) {
-                        console.log(err)
-                    });
-                }
+            console.log("-----------------------------------------\nAgregando: "+username+"  "+password+"\n-------------------------------")
+            var obj = new Object();
+            obj.name = username;
+            obj.rol_id = 1;
+            obj.password = hash;
+            var usuarionuevo= JSON.stringify(obj);      
+            console.log(usuarionuevo)
+            axios.post(api+'users/save', JSON.parse(usuarionuevo))
+            .then(function (res) {
+                console.log("se envio"+res)
+                })
+            .catch(function (err) {
+                console.log(err)
+            });
+
                 return done(null, user);
             }).catch((err) => 
             {
