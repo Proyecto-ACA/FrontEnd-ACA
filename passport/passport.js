b7b9ff9b06f4bda1137523c9154ce66295821580
CristianAlfaro <00167917@uca.edu.sv>
2021-11-09 21:29:27 -0600

    merge

--- passport/passport.js
+++ passport/passport.js
@@ -7,6 +7,7 @@ var api = 'http://localhost:3023/';
 module.exports = function (passport) {
 
     passport.serializeUser((user, done)=>{
+        console.log("hola " + user.data);
         done(null, user.data.data.id);
     });
 
@@ -21,15 +22,14 @@ module.exports = function (passport) {
         });
     });
 /*
- 
-         ██╗      ██████╗  ██████╗ ██╗███╗   ██╗
-         ██║     ██╔═══██╗██╔════╝ ██║████╗  ██║
-         ██║     ██║   ██║██║  ███╗██║██╔██╗ ██║
-         ██║     ██║   ██║██║   ██║██║██║╚██╗██║
-         ███████╗╚██████╔╝╚██████╔╝██║██║ ╚████║
-         ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝╚═╝  ╚═══╝
-                                                
- 
+
+██╗      ██████╗  ██████╗ ██╗███╗   ██╗
+██║     ██╔═══██╗██╔════╝ ██║████╗  ██║
+██║     ██║   ██║██║  ███╗██║██╔██╗ ██║
+██║     ██║   ██║██║   ██║██║██║╚██╗██║
+███████╗╚██████╔╝╚██████╔╝██║██║ ╚████║
+╚══════╝ ╚═════╝  ╚═════╝ ╚═╝╚═╝  ╚═══╝
+                                
 */
     passport.use('local-login', new LocalStrategy({
         username: 'username',
@@ -67,55 +67,50 @@ module.exports = function (passport) {
 ));
         
 /*
- 
-     ███████╗██╗ ██████╗ ███╗   ██╗    ██╗   ██╗██████╗ 
-     ██╔════╝██║██╔════╝ ████╗  ██║    ██║   ██║██╔══██╗
-     ███████╗██║██║  ███╗██╔██╗ ██║    ██║   ██║██████╔╝
-     ╚════██║██║██║   ██║██║╚██╗██║    ██║   ██║██╔═══╝ 
-     ███████║██║╚██████╔╝██║ ╚████║    ╚██████╔╝██║     
-     ╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝     ╚═════╝ ╚═╝     
-                                                                                    
- 
+
+███████╗██╗ ██████╗ ███╗   ██╗    ██╗   ██╗██████╗ 
+██╔════╝██║██╔════╝ ████╗  ██║    ██║   ██║██╔══██╗
+███████╗██║██║  ███╗██╔██╗ ██║    ██║   ██║██████╔╝
+╚════██║██║██║   ██║██║╚██╗██║    ██║   ██║██╔═══╝ 
+███████║██║╚██████╔╝██║ ╚████║    ╚██████╔╝██║     
+╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝     ╚═════╝ ╚═╝     
+
 */
 
-        passport.use('local-signup', new LocalStrategy({
-            username: 'username',
-            password: 'password',
-            passReqToCallback: true,
-        },
-        function (req, username, password, done) {
-            var salt = bcrypt.genSaltSync(10);
-            var hash = bcrypt.hashSync(password, salt);
-            var obj = new Object();
-            obj.name = username;
-            obj.rol_id = 1;
-            obj.password = hash;
-            var usuarionuevo= JSON.stringify(obj);      
-            console.log(usuarionuevo)
-            axios.post(api+'users/user',{name: username})
-                .then((user) =>
-                {
-                    //aqui se deberia validad si existe o no
-                    //----
-                    axios.post(api+'users/save', JSON.parse(usuarionuevo))
-                    .then(function (res) {
-                        return done(null, user);
-                        })
-                    .catch(function (err) {
-                        console.log(err)
-                    });
+    passport.use('local-signup', new LocalStrategy({
+        username: 'username',
+        password: 'password',
+        passReqToCallback: true,
+    },
+    function (req, username, password, done) {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(password, salt);
+        var obj = new Object();
+        obj.name = username;
+        obj.rol_id = 1;
+        obj.password = hash;
+        var usuarionuevo= JSON.stringify(obj);      
+        console.log(usuarionuevo)
+        axios.post(api+'users/user',{name: username})
+            .then((user) =>
+            {
+                //aqui se deberia validad si existe o no
+                //----
+                axios.post(api+'users/save', JSON.parse(usuarionuevo))
+                .then(function (res) {
                     return done(null, user);
-                }).catch((err) => 
-                {
-                    console.error(err);
-                    return done(null, false, {
-                        message: 'Algo salio mal'
                     })
+                .catch(function (err) {
+                    console.log(err)
+                });
+                return done(null, user);
+            }).catch((err) => 
+            {
+                console.error(err);
+                return done(null, false, {
+                    message: 'Algo salio mal'
                 })
-            }
-            
-            
-            
-        ));
-
+            })
+        }
+    ));
 }
