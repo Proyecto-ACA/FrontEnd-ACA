fd90108850b30e5a8c3def530c1e73a63030d85a
CristianAlfaro <00167917@uca.edu.sv>
2021-11-10 20:47:52 -0600

    merge

--- public/services/sign_list.js
+++ public/services/sign_list.js
@@ -2,7 +2,7 @@ var full_url = document.URL; // Get current url
 var url_array = full_url.split('=') // Split the string into an array with / as separator
 var last_segment = url_array[url_array.length-1];  // Get the last part of the array (-1)
 // alert( last_segment ); // Alert last segment
-// alert(api+'signs/getAll?id='+last_segment);
+// alert(api+'signs/getAll?id='+last_segment); 
 
 axios.get(api+'signs/getCategory?category_id='+last_segment)
       .then(function (response) {
