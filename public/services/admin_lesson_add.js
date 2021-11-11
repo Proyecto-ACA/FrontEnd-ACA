d813f72c98d8ebfb318f095f8727c9022f5627ba
luist23 <00127717@uca.edu.sv>
2021-11-10 22:53:34 -0600

    add lesson

--- public/services/admin_lesson_add.js
+++ public/services/admin_lesson_add.js
@@ -1,6 +1,22 @@
 axios.get(api+'category/getAll')
       .then(function (response) {
+          console.log('categorias', response.data);
         document.getElementById('category').innerHTML = 
+        response.data.map(function (category) 
+        {
+          return (        
+                  '<option value="'+category.id+'">'+category.name+'</option>'
+          );
+        }).join('');
+        })
+        .catch(function (err) {
+          document.getElementById('obteniendo').innerHTML = '<li class="text-danger">' + err.message + '</li>';
+        });
+
+        axios.get(api+'lesson/level/getAll')
+      .then(function (response) {
+          console.log('niveles', response.data);
+        document.getElementById('level').innerHTML = 
         response.data.map(function (category) 
         {
           return (        
@@ -19,22 +35,28 @@ axios.get(api+'category/getAll')
         var category = document.getElementById('category');
         var category_id = category.options[category.selectedIndex].value;
         var category_id_int = parseInt(category_id);
+        var level = document.getElementById('level');
+        var level_id = category.options[level.selectedIndex].value;
+        var level_id_int = parseInt(level_id);
         var image = document.getElementById('image').value;
         var description = document.getElementById('description').value;
-        var sign = document.getElementById('sign').value;
+
         var obj = new Object();
         obj.name = name;
-        obj.category_id = category_id_int;
+        obj.category = category_id_int;
+        obj.level = level_id_int
         obj.image = image;
         obj.description = description;
-        obj.sign = sign;
 
-        var jsonString= JSON.stringify(obj);      
-        axios.post('http://localhost:3023/signs/save', JSON.parse(jsonString))
+
+        console.log('objeto',obj);
+        var jsonString= JSON.stringify(obj);  
+
+        axios.post('http://localhost:3023/lesson/lesson/save', JSON.parse(jsonString))
         .then(function (res) {
           if (confirm("Se agrego con exito!\n ¿Desea regresar?")) 
           {
-            window.open("/admin/words","_self");
+            window.open("/admin/lesson","_self");
           } else {
             
           }
