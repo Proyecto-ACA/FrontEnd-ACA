2782f574eee491b94c46d300ef947299d1b7d5dd
erikalilianar22 <00106416@uca.edu.sv>
2022-06-30 16:34:40 -0600

    arreglando lista de lecciones

--- public/services/lesson_list.js
+++ public/services/lesson_list.js
@@ -7,7 +7,7 @@ axios.get(api+'lesson/lesson/getAll')
             return (
             '<div class="grid-item">'+
                 '<div class="card" style="width: 20rem; height: 20rem">'+
-                    '<a class="contenedor" href="/leccion/leccion_item?id='+category.id+'"><img class="card-img-top" src="'+category.image+'" alt="Card image cap"/>'
+                `<a class="contenedor" href="/leccion/leccion_item?id=${category.id}"><img class="card-img-top" src="${category.image}" alt="Card image cap"/>`
                         + '<div class="centrado">'
                             +'<h5>'+category.name+'</h5>'
                         +'</div></a></div>'
